9fc857d8c160ef58093ba4f97048cfff5f246e7a
Andreas Madsen <amwebdk@gmail.com>
2014-11-15 21:43:06 +0100

    [test] the test

--- test.js
+++ test.js
@@ -1,3 +1,24 @@
 
 var os = require('os');
 var test = require('tap').test;
+var xorshift = require('xorshift');
+
+var reference = require('./reference.json');
+
+function hexview(arr) {
+  var a = arr[0].toString(16);
+  var b = arr[1].toString(16);
+
+  a = (new Array(9 - a.length)).join(0) + a;
+  b = (new Array(9 - b.length)).join(0) + b;
+
+  return (a + b).toUpperCase();
+}
+
+test('state [1, 2]', function (t) {
+  for (var i = 0; i < reference.length; i++) {
+    t.strictEqual(hexview(xorshift()), reference[i]);
+  }
+
+  t.end();
+});
