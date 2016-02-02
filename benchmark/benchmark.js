025e4083733910a6f5484c34fe5ac4b5cf79d1ee
Kirill Fomichev <fanatid@ya.ru>
2016-02-02 09:10:42 +0500

    Update benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,11 +1,28 @@
-var rng = require('../');
+var benchmark = require('benchmark');
+var xorshift = require('../');
 
-var n = process.argv[2] || 25e7;
+benchmark.options.minTime = 1;
 
-var tick = process.hrtime();
-while (n--) {
-  rng.randomInt();
+/**
+ * @param {{rng: function}} rng
+ * @return {function}
+ */
+function getBenchmarkFunction (rng) {
+  return function () {
+    rng.randomInt();
+  };
 }
-var tock = process.hrtime(tick);
 
-console.log(tock[0] * 1e3 + tock[1] * 1e-6 + 'ms');
+new benchmark.Suite({
+  onStart: function () {
+    console.log('--------------------------------------------------');
+  },
+  onCycle: function (event) {
+    console.log(String(event.target));
+  },
+  onComplete: function () {
+    console.log('==================================================');
+  }
+})
+.add('xorshift128+', getBenchmarkFunction(new xorshift.XorShift128Plus([0, 1, 0, 2])))
+.run();
