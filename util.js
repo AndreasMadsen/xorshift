816a79b418cf957001ed2a7ae3734cb016813743
Kirill Fomichev <fanatid@ya.ru>
2016-02-10 23:15:54 +0300

    Add util.js
    Include randomInt64, randomSafeInt, randomBytes

---
+++ util.js
@@ -0,0 +1,32 @@
+/**
+ * @param {XorShift} prng
+ * @return {U: number, L: number}
+ */
+exports.randomInt64 = function randomInt64 (prng) {
+  var data = prng._random();
+  return {U: data[0], L: data[1]};
+};
+
+/**
+ * @param {XorShift} prng
+ * @return {number}
+ */
+exports.randomSafeInt = function randomSafeInt (prng) {
+  var data = prng._random();
+  return (data[0] & 0x001fffff) * 0xffffffff + data[1];
+};
+
+/**
+ * @param {XorShift} prng
+ * @param {number} size
+ * @return {Buffer}
+ */
+exports.randomBytes = function randomBytes (prng, size) {
+  var buffer = new Buffer(size);
+  for (var offset = 0; offset < size; offset += 8) {
+    var s = prng._random();
+    buffer.writeUInt32BE(s[0], offset, true);
+    buffer.writeUInt32BE(s[1], offset + 4, true);
+  }
+  return buffer;
+};
