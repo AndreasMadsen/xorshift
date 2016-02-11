f4e83626c888b768189253912ee886d4355999b6
Kirill Fomichev <fanatid@ya.ru>
2016-02-11 10:08:51 +0300

    Add base class XorShift

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ var XorShift128Plus = require('./lib/xorshift128plus');
 
 // There is nothing particularly scientific about this seed,
 //   it is just based on the clock.
-var rng = new XorShift128Plus([
+var prng = new XorShift128Plus([
   0, Date.now() / 65536,
   0, Date.now() % 65536
 ]);
 
-// Perform 20 iterations in the RNG,
+// Perform 20 iterations in the PRNG,
 //   this prevens a short seed from generating pseudo predictable number.
 for (var i = 0; i < 20; i++) {
-  rng._random();
+  prng._random();
 }
 
-module.exports = rng;
+module.exports = prng;
+module.exports.XorShift = require('./lib/xorshift');
 module.exports.XorShift128Plus = XorShift128Plus;
