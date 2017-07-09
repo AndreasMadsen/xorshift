7c166d77d800b6c1d5874d2a8cef8d2bab281ddf
Charles Wall <CharlesTWall3@gmail.com>
2017-07-09 00:07:39 -0400

    [fix] seed with Math.random() to prevent seed collision (#10)

--- xorshift.js
+++ xorshift.js
@@ -121,18 +121,13 @@ XorShift.prototype.random = function() {
   return d - 1;
 };
 
-// There is nothing particularly scientific about this seed, it is just
-// based on the clock.
+// Seed with Math.random() by default to prevent seed collision
+function getRandomSeed() {
+    return Math.random() * Math.pow(2, 32);
+}
 module.exports = new XorShift([
-  0, Date.now() / 65536,
-  0, Date.now() % 65536
+  getRandomSeed(),
+  getRandomSeed(),
+  getRandomSeed(),
+  getRandomSeed()
 ]);
-
-// Perform 20 iterations in the RNG, this prevens a short seed from generating
-// pseudo predictable number.
-(function () {
-  var rng = module.exports;
-  for (var i = 0; i < 20; i++) {
-    rng.randomint();
-  }
-})();
